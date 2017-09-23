1d33aeb278c0b58dc39551fbec69b9baba03267d
Bailey Cosier <bailey.cosier@gmail.com>
2017-09-23 20:56:14 +0700

    Update test runner

--- bin/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT=$( cd $BIN/../ && pwd )
-
-$(npm bin)/mocha-webpack \
-  --webpack-config $ROOT/build/webpack.config.test.js \
-  --watch --glob '*.spec.js' tests
