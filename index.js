744c3c48d54a21b40c36a580459ed685eabbcb3f
Bailey Cosier <bailey.cosier@gmail.com>
2017-10-07 05:12:48 +0700

    Export ast interfaces

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const cli = require('./lib/cli');
 const abstract = require('./lib/abstractor');
 
+const ast_from_file = abstract.ast_from_file;
+const ast_from_text = abstract.ast_from_text;
+const ast_from_stream = abstract.ast_gen;
+
 module.exports = {
-  abstract,
+  ast_from_file,
+  ast_from_text,
+  ast_from_stream,
   cli
 }
 
